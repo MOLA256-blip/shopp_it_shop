17f8fe6eab56d9dd25043f3ae4400ac78a304511
= <=>
2025-10-22 02:20:08 -0700

    first commit

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000"
+export const BASE_URL = import.meta.env.VITE_BASE_URL || "http://127.0.0.1:8000"
 
 const api = axios.create({
     baseURL: BASE_URL
